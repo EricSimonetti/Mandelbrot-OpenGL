6426a730ef72dc70a9d24311e92d21bb665600e1
EricSimonetti <56443205+EricSimonetti@users.noreply.github.com>
2021-03-19 20:50:30 -0400

    initial commit

---
+++ cmake-build-debug/CMakeFiles/Mandelbrot_OpenGL.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Mandelbrot_OpenGL.dir/InitShader.cpp.o"
+  "CMakeFiles/Mandelbrot_OpenGL.dir/main.cpp.o"
+  "Mandelbrot_OpenGL.exe"
+  "Mandelbrot_OpenGL.pdb"
+  "libMandelbrot_OpenGL.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Mandelbrot_OpenGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
